1045b7c3d41790e5e19def18e2bf0bb8f8535256
Fernando Colón <fera@friday>
2024-04-30 16:35:30 -0600

    se actualiza Package.java

--- src/Package.java
+++ src/Package.java
@@ -1,48 +1,62 @@
 package src;
 
-public class Package{
+import java.lang.Comparable;
+
+public class Package implements Comparable<Package>{
     
-	private String name;
-	private String description;
-	private double weight;
-	private int id;
+    private String name;
+    private String description;
+    private Double weight;
+    private int id;
 	
-	public Package(String name,String description,double weight,int id){
-		this.name = name;
-		this.description = description;
-		this.weight = weight;
-		this.id = id;
-	}
+    public Package(String name,String description,double weight,int id){
+	this.name = name;
+	this.description = description;
+	this.weight = weight;
+	this.id = id;
+    }
 	
-	public void setName(String name){
-		this.name = name;
-	}
+    public void setName(String name){
+	this.name = name;
+    }
 	
-	public void setDescription(String description){
-		this.description = description;
-	}
+    public void setDescription(String description){
+	this.description = description;
+    }
 	
-	public void setWeight(double weight){
-		this.weight = weight;
-	}
+    public void setWeight(double weight){
+	this.weight = weight;
+    }
 	
-	public void setId(int id){
-		this.id = id;
-	}
+    public void setId(int id){
+	this.id = id;
+    }
 	
-	public String getName(){
-		return name;
-	}
+    public String getName(){
+	return name;
+    }
 	
-	public String getDescription(){
-		return description;
-	}
+    public String getDescription(){
+	return description;
+    }
 	
-	public double getWeight(){
-		return weight;
-	}
+    public double getWeight(){
+	return weight;
+    }
 	
-	public int getId(){
-		return id;
-	}
+    public int getId(){
+	return id;
+    }
+
+    @Override
+    public String toString(){
+	return name + id;
+    }
+
+    @Override
+    public int compareTo(Package p){
+	return this.weight.compareTo(p.weight);
+    }
+
+    
 }
